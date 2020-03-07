98b59d927cf0d8f0ec8519e4206e56959635c187
AndreyBMWX6 <a_s_bykov@mail.ru>
2020-03-07 14:40:54 +0300

    first commit

---
+++ LR3/LR3.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include "Point.h"
+#include "Point3D.h"
+
+int main()
+{
+	Point p1(4.56, 8.91);
+	Point3D p2(5.56,7.77,4.867);
+	Point* pp;
+	pp = &p1;
+	pp->print();
+	std::cout << "\n\n";
+	pp = &p2;
+	pp->print();
+}
